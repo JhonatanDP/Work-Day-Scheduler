382ea57471456cd06d4a763861abf04abe59b113
Jhonatan <jonathandiazp@gmail.com>
2022-07-10 17:16:02 -0400

    html done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
+var tasks = {};
+
+
 var currentDay = moment().format('dddd, MMMM Do');
 $("#currentDay").html(currentDay);
-console.log(currentDay);
 
+
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+    // if nothing in localStorage, create a new object to track all task status arrays
+    if (!tasks) {
+      tasks = {
+        task: [],
+        hour: []
+      };
+    }
+  
+    // loop over object properties  
+    $.each(tasks, function(list, arr) {
+      // then loop over sub-array
+      arr.forEach(function(task) {
+        createTask(task.text, task.hour, list);
+      });
+    });
+  };
+
+// var tasks = {
+//     task : "THIS",
+//     time: "9PM"
+// }
+
+// localStorage.setItem("tasks", JSON.stringify(tasks));
